ba66f6a8d78207831fa10d36f273f0516f9d0f64
Jahongir2109 <jahongirachilov004@gmail.com>
2022-12-21 00:37:34 +0500

    task fixed

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,6 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        System.out.println(temperatureCelsius*5/9+32);
+        System.out.println((double) temperatureCelsius*((double) 9/(double) 5) +32);
     }
 }
