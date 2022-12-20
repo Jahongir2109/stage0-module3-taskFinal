8a09f9b7257a62d50f7d10b2f86662ff5c5fceee
Jahongir2109 <jahongirachilov004@gmail.com>
2022-12-21 00:46:31 +0500

    fixed positive

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        System.out.println(number%2!=0);
+        System.out.println(number%2!=0 || number%2==1);
     }
 }
